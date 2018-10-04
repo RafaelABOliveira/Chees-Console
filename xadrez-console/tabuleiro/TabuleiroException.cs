f361c42affc74be506e8d92d96174a90c52a2a18
Rafael Oliveira <rafaelntn766@gmail.com>
2018-10-04 10:39:41 -0300

    Tratamento de erros com excecao personalizada

---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace tabuleiro {
+    class TabuleiroException : Exception {
+
+        public TabuleiroException(string msg) : base(msg) {
+
+        }
+
+        [Serializable]
+        internal class e : Exception {
+            public e() {
+            }
+
+            public e(string message) : base(message) {
+            }
+
+            public e(string message, Exception innerException) : base(message, innerException) {
+            }
+
+            protected e(SerializationInfo info, StreamingContext context) : base(info, context) {
+            }
+        }
+    }
+}
